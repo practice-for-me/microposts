428271b7d28bca321d5b2704c6242054d688fc43
suemitu <suemiken@icloud.com>
2016-05-01 14:42:08 +0000

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  def create_table
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module Microposts
@@ -18,16 +19,17 @@ module Microposts
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
     end
+    
+    
   end
 end
