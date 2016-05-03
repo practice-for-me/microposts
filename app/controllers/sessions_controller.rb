a07a7511dc4d8b78255e3b4fd3fae49cd5ce0d7c
suemitu <suemiken@icloud.com>
2016-05-03 06:39:07 +0000

    logout and login can

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    #もしユーザが入らなかったらnilが入る　この場合emailからユーザを探している
+    @user = User.find_by(email: params[:session][:email].downcase)
+    # @user!=nil &&  フォームで入力したpassword == 登録されたユーザのカラムであるpassword
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:info] = "logged in as #{@user.name}"
+      redirect_to @user
+    else
+      #警告のフラッシュメッセージ
+      flash[:danger] = 'invalid email/password combination'
+      #newテンプレートの表示
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
