cd728e98aac2f45260f4c7a2c405e2bac9db8314
suemitu <suemiken@icloud.com>
2016-05-03 12:17:10 +0000

    create edit and edit can

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -17,9 +19,26 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to user_path , notice: 'プローフィールを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 #保存される直前に実行
   before_save { self.email = self.email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
