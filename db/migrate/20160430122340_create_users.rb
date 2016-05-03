ebdc53c0c61974dd4c75b2f99f75f617684843d2
suemitu <suemiken@icloud.com>
2016-05-03 13:38:51 +0000

    live_place add

--- db/migrate/20160430122340_create_users.rb
+++ db/migrate/20160430122340_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
-      t.string :live
+      t.string :live_place
 
       t.timestamps null: false
     end
