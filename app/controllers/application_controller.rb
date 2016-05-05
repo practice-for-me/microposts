43d077a08ca43a99d3fe70f7204c74265206264f
suemitu <suemiken@icloud.com>
2016-05-05 07:29:07 +0000

    syusei

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   
   private
-  def loggend_in_user
+  def logged_in_user
     unless logged_in?
       store_location
       flash[:danger] = "Please log in."
