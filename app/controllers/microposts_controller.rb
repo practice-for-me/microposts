939fb67589b660dbad741e9e9994e6b7e4693d99
suemitu <suemiken@icloud.com>
2016-05-08 12:02:37 +0000

    validate update and not sending update

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
             flash[:success] = "Micropost created!"
             redirect_to root_url
         else
-            render 'static_pages.home'
+            render 'static_pages/home'
         end
     end
     
